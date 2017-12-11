d337ea7789678a2dd978c843d56c3f48b31db108
Giancarlos Salas <giansalex@gmail.com>
2017-12-10 19:32:26 -0500

    php 7.2 features

--- code72.php
+++ code72.php
@@ -1,1 +1,27 @@
 <?php
+
+// Type object
+function test(object $obj) : object
+{
+    return new SplQueue();
+}
+
+test(new StdClass());
+
+$ab = 'as';
+$ac = 'ac';
+
+sodium_add($ab, $ac);
+
+
+interface A
+{
+    public function Test(array $input);
+}
+
+class B implements A
+{
+    public function Test($input){} // type omitted for $input
+}
+
+
