316de173e5c05cf043c41f1c8613b43f00f24f4f
giansalex <giansalex@gmail.com>
2017-12-15 16:15:21 -0500

    update 7.1

--- code71.php
+++ code71.php
@@ -13,12 +13,40 @@ function sum(?int $a, ?int $b): ?int
 var_dump(sum(null, null));
 
 interface Fooable {
-    function foo(): ?string;
+    public function foo(): ?string;
 }
 interface StrictFooable extends Fooable {
-    function foo(): string; // valid
+    public function foo(): string; // valid
 }
 
+trait MoveTrait {
+    public $action;
+    public function move(int $value): void {
+
+    }
+}
+interface Action {
+    public function run(): void;
+}
+
+class Person implements Action {
+
+    use MoveTrait {
+        MoveTrait::move as protected movePerson;
+        MoveTrait::move as protected;
+    }
+    public function run(): void
+    {
+        // TODO: Implement run() method.
+    }
+}
+
+class Child extends Person {
+    public function build()
+    {
+        $this->movePerson(2);
+    }
+}
 function dump(iterable $items)
 {
     var_dump($items);
@@ -62,3 +90,8 @@ pcntl_signal(SIGHUP,  function($sig) {
 });
 
 posix_kill(posix_getpid(), SIGHUP);
+
+$person = new Person();
+if ($person instanceof Action) {
+
+}
